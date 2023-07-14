af784b5bf37886f5e519a2094d88155ee4b4c455
Outsider <outsideris@gmail.com>
2023-07-14 20:06:37 +0900

    match youtube badge size
    Signed-off-by: Outsider <outsideris@gmail.com>

--- components/badges.tsx
+++ components/badges.tsx
@@ -7,8 +7,8 @@ export default function Badges({ link }) {
             <Link href={link}>
                 <Image src="/badges/apple.svg" alt="Listen on Apple Podcasts" width={300} height={0} priority className="badge" />
             </Link>
-            <Link href={link}>
-                <Image src="/badges/youtube.svg" alt="Available on YouTube" width={300} height={0} priority className="badge" />
+            <Link href={link} className="youtube">
+                <Image src="/badges/youtube.svg" alt="Available on YouTube" width={240} height={0} priority className="badge youtube" />
             </Link>
             <Link href={link}>
                 <Image src="/badges/google.svg" alt="Listen on Google Podcasts" width={300} height={0} priority className="badge" />
