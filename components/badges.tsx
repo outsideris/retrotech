606147af4dd7e1fb883bcdc5bcf74aab51db54e9
Outsider <outsideris@gmail.com>
2023-07-14 16:37:45 +0900

    extract badges as components
    Signed-off-by: Outsider <outsideris@gmail.com>

---
+++ components/badges.tsx
@@ -0,0 +1,21 @@
+import Link from 'next/link'
+import Image from 'next/image'
+
+export default function Badges({ link }) {
+    return (
+        <div className="badges">
+            <Link href={link}>
+                <Image src="/badges/apple.svg" alt="Listen on Apple Podcasts" width={300} height={0} priority className="badge" />
+            </Link>
+            <Link href={link}>
+                <Image src="/badges/youtube.svg" alt="Available on YouTube" width={300} height={0} priority className="badge" />
+            </Link>
+            <Link href={link}>
+                <Image src="/badges/google.svg" alt="Listen on Google Podcasts" width={300} height={0} priority className="badge" />
+            </Link>
+            <Link href={link}>
+                <Image src="/badges/spotify.svg" alt="Listen on Spotify" width={300} height={0} priority className="badge" />
+            </Link>
+        </div>
+    )
+}
