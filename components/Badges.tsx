7c62cf63b0438aade5a532739e2f83352bd8f1e4
Outsider <outsideris@gmail.com>
2023-07-27 21:41:48 +0900

    show google podcast
    Signed-off-by: Outsider <outsideris@gmail.com>

--- components/Badges.tsx
+++ components/Badges.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Badges({
   apple = "https://podcasts.apple.com/kr/podcast/retrotech-%ED%8C%9F%EC%BA%90%EC%8A%A4%ED%8A%B8/id1698903712",
   youtube = "https://www.youtube.com/@OutsidersDevStory/podcasts",
-  google = "",
+  google = "https://podcasts.google.com/feed/aHR0cHM6Ly9yZXRyb3RlY2gub3V0c2lkZXIuZGV2L2ZlZWQueG1s",
   spotify = "https://open.spotify.com/show/3nSplj43Rd86snTrsEHdTI"
 }) {
     return (
@@ -15,9 +15,9 @@ export default function Badges({
             <Link href={youtube} className="youtube">
                 <Image src="/badges/youtube.svg" alt="Available on YouTube" width={240} height={0} priority className="badge youtube" />
             </Link>
-            {/*<Link href={google}>*/}
-            {/*    <Image src="/badges/google.svg" alt="Listen on Google Podcasts" width={300} height={0} priority className="badge" />*/}
-            {/*</Link>*/}
+            <Link href={google}>
+                <Image src="/badges/google.svg" alt="Listen on Google Podcasts" width={300} height={0} priority className="badge" />
+            </Link>
             <Link href={spotify}>
                 <Image src="/badges/spotify.svg" alt="Listen on Spotify" width={300} height={0} priority className="badge spotify" />
             </Link>
