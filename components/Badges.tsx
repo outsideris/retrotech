acdc9a912f777f7fb1c1e2b4efadd0d74a58a0f3
Outsider <outsideris@gmail.com>
2024-01-20 15:36:09 +0900

    replace google podcast with rss feed badge
    google podcast will shutdown in 2024
    
    Signed-off-by: Outsider <outsideris@gmail.com>

--- components/Badges.tsx
+++ components/Badges.tsx
@@ -4,7 +4,8 @@ import Image from 'next/image'
 export default function Badges({
   apple = "https://podcasts.apple.com/kr/podcast/retrotech-%ED%8C%9F%EC%BA%90%EC%8A%A4%ED%8A%B8/id1698903712",
   youtube = "https://www.youtube.com/playlist?list=PLEHf_UYxvkp9HCnP3UIZhEss_Yo4XuUDX",
-  google = "https://podcasts.google.com/feed/aHR0cHM6Ly9yZXRyb3RlY2gub3V0c2lkZXIuZGV2L2ZlZWQueG1s",
+  google,
+  rss = "/feed.xml",
   spotify = "https://open.spotify.com/show/3nSplj43Rd86snTrsEHdTI"
 }) {
     return (
@@ -15,12 +16,19 @@ export default function Badges({
             <Link href={youtube} className="youtube">
                 <Image src="/badges/youtube.svg" alt="Available on YouTube" width={240} height={0} priority className="badge youtube" />
             </Link>
-            <Link href={google}>
-                <Image src="/badges/google.svg" alt="Listen on Google Podcasts" width={300} height={0} priority className="badge" />
-            </Link>
             <Link href={spotify}>
-                <Image src="/badges/spotify.svg" alt="Listen on Spotify" width={300} height={0} priority className="badge spotify" />
+              <Image src="/badges/spotify.svg" alt="Listen on Spotify" width={300} height={0} priority className="badge spotify" />
             </Link>
+            {google &&
+              <Link href={google}>
+                <Image src="/badges/google.svg" alt="Listen on Google Podcasts" width={300} height={0} priority className="badge" />
+              </Link>
+            }
+            {!google &&
+              <Link href={rss}>
+                <Image src="/badges/rss.svg" alt="Get the RSS Feed" width={300} height={0} priority className="badge" />
+              </Link>
+            }
         </div>
     )
 }
